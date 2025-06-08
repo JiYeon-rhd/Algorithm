e538df07b1a2cc0f50a1e22a192cf9a0c53b44ff
Ji Yeon Kim <88575220+JiYeon-rhd@users.noreply.github.com>
2025-06-08 18:40:12 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,10 @@
 -- 코드를 입력하세요
-SELECT sub.FLAVOR
-FROM(
-    SELECT f.FLAVOR, sum(f.TOTAL_ORDER) + sum(j.TOTAL_ORDER) as TOTAL_ORDER
-    FROM FIRST_HALF as f join (SELECT FLAVOR, sum(TOTAL_ORDER) as TOTAL_ORDER
-                            FROM JULY
-                            GROUP BY FLAVOR) as j 
+SELECT f.FLAVOR
+FROM FIRST_HALF as f join 
+    (SELECT FLAVOR, sum(TOTAL_ORDER) as JULY_TOTAL_ORDER
+    FROM JULY
+    GROUP BY FLAVOR) as j
     on f.FLAVOR = j.FLAVOR
-    GROUP BY f.FLAVOR
-) as sub
-ORDER BY sub.TOTAL_ORDER DESC
+ORDER BY (f.TOTAL_ORDER + j.JULY_TOTAL_ORDER) DESC
 limit 3
+    
