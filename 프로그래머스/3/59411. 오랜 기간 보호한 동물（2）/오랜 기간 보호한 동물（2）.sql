945ff2b19574cfbd7bb836a570d5665cb72dfe43
Ji Yeon Kim <88575220+JiYeon-rhd@users.noreply.github.com>
2025-06-10 22:38:39 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -2,7 +2,7 @@
 -- 입양을 간 동물 중, 보호기간이 가장 길었던 동물 두마리!!
 -- 보호 기간이 긴 순으로 
 SELECT o.ANIMAL_ID, o.NAME
-FROM ANIMAL_OUTS as o join (SELECT ANIMAL_ID, DATETIME FROM ANIMAL_INS) as i
+FROM ANIMAL_OUTS as o join ANIMAL_INS as i
         on o.ANIMAL_ID = i.ANIMAL_ID
 ORDER BY (DATEDIFF(o.DATETIME, i.DATETIME) + 1)  DESC
 limit 2
