d8e65935951b311e4b4be9ef68587a7a3db1b2b9
Ji Yeon Kim <88575220+JiYeon-rhd@users.noreply.github.com>
2025-06-10 22:21:29 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+-- Lucy, Ella, Pickle, Rogan, Sabrina, Mitty
+-- 
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
