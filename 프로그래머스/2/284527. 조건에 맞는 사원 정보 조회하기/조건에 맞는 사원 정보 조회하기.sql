11258d104094e1cc1774dc9bafd7fdb20ac1c8a1
Ji Yeon Kim <88575220+JiYeon-rhd@users.noreply.github.com>
2025-06-11 16:19:13 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,13 +1,18 @@
 -- 코드를 작성해주세요
-with EMP_SCORE AS (
+-- 2022년도 평가 점수 가장 높은 사원만 정보 조회
+-- 상하반기 점수 합
+
+with TOP_SCORE as(
     SELECT EMP_NO, sum(SCORE) as SCORE
     FROM HR_GRADE
+    WHERE YEAR = 2022
     GROUP BY EMP_NO
-    ),
-MAX_SCORE as(
-    SELECT EMP_NO, SCORE
-    FROM EMP_SCORE
-    WHERE SCORE in (SELECT max(SCORE) FROM EMP_SCORE)
-) 
-SELECT m.SCORE, h.EMP_NO, h.EMP_NAME, h.POSITION, h.EMAIL
-FROM HR_EMPLOYEES as h join MAX_SCORE as m on h.EMP_NO = m.EMP_NO
+)
+SELECT s.SCORE, e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+FROM HR_EMPLOYEES as e left join TOP_SCORE as s on e.EMP_NO = s.EMP_NO
+WHERE s.SCORE = (SELECT max(SCORE) FROM TOP_SCORE) 
+
+
+            
+            
+            
